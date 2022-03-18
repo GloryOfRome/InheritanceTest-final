920ede2c864acd655105292ab0b3d51b89f2ab32
Yanjun Yu <yanjunyu@student.mitt.ca>
2022-03-17 19:28:03 -0500

    在种子文件中添加三组数据，每次运行程序会进入数据库，但是再次运行需要把数据删除，否则会重复

--- InheritanceTest-final/Models/SeedData.cs
+++ InheritanceTest-final/Models/SeedData.cs
@@ -1,4 +1,5 @@
 ﻿using InheritanceTest_final.Data;
+using InheritanceTest_final.Models;
 using Microsoft.EntityFrameworkCore;
 
 namespace InheritanceTest_final
@@ -10,6 +11,35 @@ namespace InheritanceTest_final
             var context = new InheritanceTest_finalContext(serviceProvider.GetRequiredService<DbContextOptions<InheritanceTest_finalContext>>());
             //GetRequiredService如果没有serviceType类型的服务，则抛出一个InvalidOperationException异常
             //InheritanceTest_finalContext这两个地方填代码在数据库中的文件名
+
+            Question newQuestion = new Question
+            {
+                Title = "测试问题",
+                Body = "我如何修复我的系统，当它坏了的时候"
+            };
+            context.Question.Add(newQuestion);//在context中添加newQuestion
+            context.SaveChanges();//将改变保持
+
+            Comment newComment = new Comment
+            {
+                Body = "你试过谷歌没有",
+                Question = newQuestion
+            };
+            newQuestion.Comments.Add(newComment);
+            context.Comment.Add(newComment);
+            context.SaveChanges();//将改变保持
+
+            Answer newAnswer = new Answer
+            {
+                Body = "卸载你的系统",
+                Question = newQuestion,
+                IsSelectedAnswer = true//选择答案
+            };
+            newQuestion.Comments.Add(newAnswer);
+            context.Answer.Add(newAnswer);
+
+
+            context.SaveChanges();//将改变保持
         }
     }
 }
