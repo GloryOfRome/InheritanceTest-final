83263e0ff4aa3700241483ff8722801d2f56f346
Yanjun Yu <yanjunyu@student.mitt.ca>
2022-03-17 19:39:44 -0500

    添加class（评分）继承Comment

---
+++ InheritanceTest-final/Models/Review.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace InheritanceTest_final.Models
+{
+    public class Review : Comment//评价，打分
+    {
+        [Range(1, 10)]
+        public int Score { get; set; }//得分
+    }
+}
