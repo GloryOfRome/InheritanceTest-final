b0fffaf16ff94d859341e39b6a3b8a511d314245
Yanjun Yu <yanjunyu@student.mitt.ca>
2022-03-17 19:19:24 -0500

    1.建立Seed Data种子数据 2.在program中添加临时导入代码

---
+++ InheritanceTest-final/Models/SeedData.cs
@@ -0,0 +1,10 @@
+﻿namespace InheritanceTest_final
+{
+    public class SeedData//种子数据
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+
+        }
+    }
+}
--- InheritanceTest-final/Program.cs
+++ InheritanceTest-final/Program.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using InheritanceTest_final.Data;
+using InheritanceTest_final;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddDbContext<InheritanceTest_finalContext>(options =>
@@ -10,6 +12,11 @@ builder.Services.AddDbContext<InheritanceTest_finalContext>(options =>
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    SeedData.Initialize(services);
+}
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
