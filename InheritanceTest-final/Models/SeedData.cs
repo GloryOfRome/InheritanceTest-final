81eceb8f664e29b83942b769e88f05f1b698f38c
Yanjun Yu <yanjunyu@student.mitt.ca>
2022-03-17 19:22:57 -0500

    InheritanceTest_finalContext这两个地方填代码在数据库中的文件名

--- InheritanceTest-final/Models/SeedData.cs
+++ InheritanceTest-final/Models/SeedData.cs
@@ -1,10 +1,15 @@
-﻿namespace InheritanceTest_final
+﻿using InheritanceTest_final.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace InheritanceTest_final
 {
     public class SeedData//种子数据
     {
         public static void Initialize(IServiceProvider serviceProvider)
         {
-
+            var context = new InheritanceTest_finalContext(serviceProvider.GetRequiredService<DbContextOptions<InheritanceTest_finalContext>>());
+            //GetRequiredService如果没有serviceType类型的服务，则抛出一个InvalidOperationException异常
+            //InheritanceTest_finalContext这两个地方填代码在数据库中的文件名
         }
     }
 }
